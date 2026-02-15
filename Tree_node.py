bfd3f489fb4d59d99ccb69f79e593b48d8af13e8
Jorbat Collins <jbcollins254@gmail.com>
2026-02-15 17:47:23 +0000

    adds search functionality

--- Tree_node.py
+++ Tree_node.py
@@ -15,7 +15,7 @@ class BinarySearchTree:
             self.root = BinaryTree(data)
         else:
             self._insert_recursive(self.root, data)
-            
+# helper for insert   
     def _insert_recursive(self, node, data):
         if data < node.data:
             if node.left is None:
@@ -27,5 +27,19 @@ class BinarySearchTree:
                 node.right = BinaryTreeNode(data)
             else:
                 self._insert_recursive(node.right, data)
+
+    def search(self,data):
+        return self._search_recursive(self.root, data)
+
+    # helper for search function
+    def _search_recursive(self, node, data):
+        if node is None:
+            return False
+        if data == node.data:
+            return True
+        elif data < node.data:
+            return self._search_recursive(node.left, data)
+        else:
+            return self._search_recursive(node.right, data)
 
 
