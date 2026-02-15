2317ae8e8b6ce3754336efe45765aa25a8fbd005
Jorbat Collins <jbcollins254@gmail.com>
2026-02-15 18:18:40 +0000

    initialization of graph

---
+++ Graph.py
@@ -0,0 +1,8 @@
+# graphs are the structures that model relationships
+class Graph:
+    def __init__ (self, directed=False ):
+        
+        self.graph = {} # this uses the adjacency list rep
+        self.derected = derected
+
+    
